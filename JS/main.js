37bc991c1caee335c56553a17197d2210491e84d
enricorenna01 <erenna39@gmail.com>
2024-11-23 05:34:36 +0100

    feat: added variables

---
+++ JS/main.js
@@ -0,0 +1,26 @@
+//Definisco gli input necessari
+
+
+//Definisco una variabile kmNumber e la trasformo da stringa a numero
+const kmNumber = Number(prompt("Inserisci il numero dei km che vuoi percorrere"));
+console.log(kmNumber);
+
+//Definisco una variabile userAge e la trasformo da stringa a numero
+const userAge = Number(prompt("Inserisci la tua età"));
+console.log(userAge);
+
+//Salvo una variabile kmPrice che sarebbe il prezzo al km
+const kmPrice = 0.21;
+
+//Salvo una variabile underAge per determinare il valore dell'età di un minorenne
+const underAge = 18;
+
+//Salvo una variabile overAge per determinare il valore dell'età dai 65 anni in sù
+const overAge = 65;
+
+//Salvo una variabile underDiscount per determinare il valore sconto dei minorenni
+const underDiscount = 20;
+
+//Salvo una variabile underDiscount per determinare il valore sconto dei over 65
+const underDiscount = 40;
+
